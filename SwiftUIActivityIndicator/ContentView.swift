c2aa503b0bcb6294b6aecfb7010d526de0d5f446
Yisselda <yisselda@defmethod.io>
2020-06-11 22:06:56 -0400

    Initial Commit

---
+++ SwiftUIActivityIndicator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIActivityIndicator
+//
+//  Created by Yisselda Rhoc on 6/11/20.
+//  Copyright © 2020 Yisselda Rhoc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
